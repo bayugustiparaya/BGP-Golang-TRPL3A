6b490cd7d36c9fe163d900c73f21c6821ba93d02
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-11-04 23:04:59 +0700

    add struct customer and product

--- Framework/git/order/common/global.go
+++ Framework/git/order/common/global.go
@@ -33,4 +33,32 @@ type Result struct {
 	Remark string `json:"remark,omitempty"`
 }
 
+type Customer struct {
+	CustomerID   string `json:"customerID"`
+	CompanyName  string `json:"companyName"`
+	ContactName  string `json:"contactName"`
+	ContactTitle string `json:"contactTitle"`
+	Address      string `json:"address"`
+	City         string `json:"city"`
+	Region			 string `json:"region"`
+	PostalCode   string `json:"postalCode"`
+	Country      string `json:"country"`
+	Phone        string `json:"phone"`
+	Fax					 string `json:"fax"`
+}
+
+type Product struct {
+	ProductID				int			`json:"productID"`
+	ProductName			string	`json:"productName"`
+	SupplierID			int			`json:"supplierID"`
+	CategoryID			int			`json:"categoryID"`
+	QuantityPerUnit	string	`json:"quantityPerUnit"`
+	UnitPrice				float32	`json:"unitPrice"`
+	UnitInStock			int			`json:"unitInStock"`
+	UnitOnOrder			int			`json:"unitOnOrder"`
+	ReorderLevel		int			`json:"reorderLevel"`
+	Discontinued		int8		`json:"discontinued"`
+	Description			string	`json:"description"`
+}
+
 //End Struct API
