478387d3c189916d6e64c0539738ce9241fedadf
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-11-04 22:10:18 +0700

    Pertemuan-7

---
+++ Framework/git/order/middleware/basicmw.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"time"
+
+	"context"
+
+	cm "BGP-Golang-TRPL3A/Framework/git/order/common"
+	"BGP-Golang-TRPL3A/Framework/git/order/services"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+
+func BasicMiddleware() services.ServiceMiddleware {
+	return func(next services.PaymentServices) services.PaymentServices {
+		return BasicMiddlewareStruct{next}
+	}
+}
+
+type BasicMiddlewareStruct struct {
+	services.PaymentServices
+}
+
+func (mw BasicMiddlewareStruct) OrderHandler(ctx context.Context, request cm.Message) cm.Message {
+	defer func(begin time.Time) {
+		log.WithField("execTime", float64(time.Since(begin).Nanoseconds())/float64(1e6)).Info("OrderHandler ends")
+	}(time.Now())
+
+	log.WithField("request", request).Info("OrderHandler begins")
+
+	return mw.PaymentServices.OrderHandler(ctx, request)
+	
+}
+
