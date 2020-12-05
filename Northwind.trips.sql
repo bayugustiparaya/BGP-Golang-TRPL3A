69953b9cb986f0680ec6daf74226ccb9c06a05db
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-12-05 10:46:49 +0700

    add northwind.trips

---
+++ Northwind.trips.sql
@@ -0,0 +1,20 @@
+
+--
+-- Database: `northwind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trips`
+--
+
+CREATE TABLE `trips` (
+  `tripId` varchar(200) NOT NULL,
+  `airlineName` varchar(200) NOT NULL,
+  `airportName` varchar(200) NOT NULL,
+  `destination` varchar(200) NOT NULL,
+  `departureDate` varchar(200) NOT NULL
+) ;
+
+
